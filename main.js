251d6367e2569c3da41b18bbe066ed5744a87a77
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-03-07 07:55:41 -0300

    atualizaçaõ js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let nome_usuario = prompt("Qual é o seu nome?");
+let nome_usuario = "";
 let elemento = document.querySelector ("#nome-usuário");
+
+
+
+while(nome_usuario == ""){
+    nome_usuario = prompt("Qual é o seu nome?");
+}
+
 
 elemento.textContent = nome_usuario; 
